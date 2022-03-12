9bc467858fa6ea840a329490d989e7b73abf3505
Thed <theodore1seriakov@gmail.com>
2022-03-12 23:25:59 +0600

    second Commit

---
+++ src/com/forCycles/ForCycles.java
@@ -0,0 +1,9 @@
+package com.forCycles;
+
+public class ForCycles {
+    public static void main(String[] args) {
+        for(int k = 1; k <=10; k++){
+            System.out.println(k * k);
+        }
+    }
+}
