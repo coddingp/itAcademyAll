2f2f3e6ab21fd6c777bc9fb0766ed9956c472a2f
Thed <theodore1seriakov@gmail.com>
2022-03-12 22:01:35 +0600

    second Commit

---
+++ src/com/concotinations/Concotinations.java
@@ -0,0 +1,12 @@
+package com.concotinations;
+
+public class Concotinations {
+
+    public static void main(String[] args) {
+        String a = "Hello, ";
+        String b = "my name is ";
+        String name = "Thed";
+        String message = a + b + name;
+        System.out.println(message);
+    }
+}
