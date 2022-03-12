8e535596606109e521ba31f40a3f77e612679dfc
Thed <theodore1seriakov@gmail.com>
2022-03-13 02:11:17 +0600

    second Commit

---
+++ src/com/tsThroughException/FileF.java
@@ -0,0 +1,16 @@
+package com.tsThroughException;//package com.helloWorld;
+import java.io.*;
+public class FileF {
+    File f = new File("test10.txt");
+    FileOutputStream fos = new FileOutputStream(f);
+    FileInputStream fis = new FileInputStream(f);
+
+    public FileF() throws FileNotFoundException {
+    }
+
+    public FileF(File f, FileOutputStream fos, FileInputStream fis) throws FileNotFoundException {
+        this.f = f;
+        this.fos = fos;
+        this.fis = fis;
+    }
+}
